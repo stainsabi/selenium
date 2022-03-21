bf3582bdc599a7256d0b5a8b8558b329df67e35e
stainsabi <43885778+stainsabi@users.noreply.github.com>
2022-03-21 13:16:04 +0530

    Update AppTest.java

--- src/test/java/com/mindtree/app/AppTest.java
+++ src/test/java/com/mindtree/app/AppTest.java
@@ -22,33 +22,6 @@ public class AppTest
         assertTrue( true );
     }
     
-    @Test
-    public void chrome() {
-    	System.setProperty("webdriver.chrome.driver", ".//driver//chromedriver.exe");
-    	WebDriver driver=new ChromeDriver();
-    	driver.get("https://www.facebook.com/");
-    	System.out.println(driver.getTitle());
-    	if("Facebook – log in or sign up".equals(driver.getTitle())) {
-    		assertTrue(true);
-    	}
-    	else {
-    		assertTrue(false);
-    	}
-    	driver.quit();
-    }
-    @Test
-    public void firefox() {
-    	System.setProperty("webdriver.gecko.driver", ".//driver//geckodriver.exe");
-    	WebDriver driver2= new FirefoxDriver();
-    	driver2.get("https://www.facebook.com/");
-    	System.out.println(driver2.getTitle());
-    	if("Facebook – log in or sign up".equals(driver2.getTitle())) {
-    		assertTrue(true);
-    	}
-    	else {
-    		assertTrue(false);
-    	}
-    	driver2.quit();
-    }
+    
    
 }
